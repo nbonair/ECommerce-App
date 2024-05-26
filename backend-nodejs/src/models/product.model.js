ddc3a331d5ff67113fc79ded8c67ef8da771aa45
nbonair <nbholdacc@gmail.com>
2024-05-26 16:53:26 +1000

    Added attributes, slug gen, fulltext index

--- backend-nodejs/src/models/product.model.js
+++ backend-nodejs/src/models/product.model.js
@@ -1,4 +1,5 @@
 const { model, Schema } = require('mongoose');
+const slugify = require('slugify')
 
 const DOCUMENT_NAME = 'Product'
 const COLLECTION_NAME = 'Products'
@@ -8,16 +9,36 @@ const productSchema = new Schema({
     product_name: { type: String, required: true },
     product_thumb: { type: String, required: true },
     product_description: String,
+    product_slug: String,
     product_price: { type: Number, required: true },
     product_quantity: { type: Number, required: true },
     product_type: { type: String, required: true, enum: ['Electronics', 'Clothing', 'Furniture'] },
     product_shop: { type: Schema.Types.ObjectId, ref: 'Shop' },
-    product_attributes: { type: Schema.Types.Mixed, required: true }
+    product_attributes: { type: Schema.Types.Mixed, required: true },
+    product_ratingsAverage: {
+        type: Number,
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be below 1.0'],
+        set: (val) => Math.round(val * 10) / 10
+    },
+    product_variations: { type: Array, default: [] },
+    isDraft: { type: Boolean, default: true, index: true, select: false },
+    isPublished: { type: Boolean, default: false, index: true, select: false }
 }, {
     collection: COLLECTION_NAME,
     timestamps: true
 })
+// Create index
+productSchema.index({ product_name: 'text', product_description: 'text' }) // Product search public
 
+
+// Document middleware: before .save and create
+
+productSchema.pre('save', function (next) {
+    this.product_slug = slugify(this.product_name, { lower: true })
+    next()
+})
 // type clothings
 
 const clothingSchema = new Schema({
