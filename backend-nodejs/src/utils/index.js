d300e04628111025185a739f17d46c3881ca2abf
nbonair <nbholdacc@gmail.com>
2024-05-27 00:29:17 +1000

    Added mongodb-select object formatter

--- backend-nodejs/src/utils/index.js
+++ backend-nodejs/src/utils/index.js
@@ -4,6 +4,17 @@ const getInfoData = ({ fields = [], object = {} }) => {
     return _.pick(object, fields)
 }
 
+const getSelectData = (select = []) => {
+    return Object.fromEntries(select.map(elem => [elem, 1]))
+}
+
+const getUnSelectData = (select = []) => {
+    return Object.fromEntries(select.map(elem => [elem, 0]))
+}
+
+
 module.exports = {
-    getInfoData
+    getInfoData,
+    getSelectData,
+    getUnSelectData
 }
